980a6e345157c48ac2a6c2b5c24f8cf118667f0d
kong <376634352@qq.com>
2023-04-24 20:23:44 +0800

    添加首页静态文件

---
+++ src/main/java/com/coder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,17 @@
+package com.coder.community.dao;
+
+import com.coder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+    //    根据userId获取评论信息
+    List<DiscussPost> selectDiscussPost(int userId, int offset, int limit);
+
+    // 获取userId在数据库中的评论行数
+    // @Param 用于给参数取别名，如果只有一个参数，并且在if里使用，则必须使用别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+}
---
+++ src/main/java/com/coder/community/service/DiscussPostService.java
@@ -0,0 +1,22 @@
+package com.coder.community.service;
+
+import com.coder.community.dao.DiscussPostMapper;
+import com.coder.community.entity.DiscussPost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DiscussPostService {
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
+        return discussPostMapper.selectDiscussPost(userId, offset, limit);
+    }
+
+    public int findDiscussPostRows(int userId) {
+        return discussPostMapper.selectDiscussPostRows(userId);
+    }
+}
