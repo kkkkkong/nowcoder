6bfa6bceca0ef17793bdba7952c57a370cbf3d92
kong <376634352@qq.com>
2023-04-24 22:26:37 +0800

    首页的分页功能实现

--- src/main/java/com/coder/community/controller/HomeController.java
+++ src/main/java/com/coder/community/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.coder.community.controller;
 
 import com.coder.community.entity.DiscussPost;
+import com.coder.community.entity.Page;
 import com.coder.community.service.DiscussPostService;
 import com.coder.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +20,17 @@ public class HomeController {
     private DiscussPostService discussPostService;
     @Autowired
     private UserService userService;
+
     @GetMapping("/index")
-    public String getIndexPage(Model model) {
-        List<DiscussPost> list = discussPostService.findDiscussPosts(0, 0, 10);
+    public String getIndexPage(Model model, Page page) {
+//        方法调用前，SpringMVC会自动实例化Model和Page，并且将page注入model
+//        所以，在thymeleaf中就可以直接访问page中的数据
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String, Object>> discussPosts = new ArrayList<>();
         if (!list.isEmpty()) {
-            for (DiscussPost post :list) {
+            for (DiscussPost post : list) {
                 Map<String, Object> map = new HashMap<>();
                 map.put("post", post);
                 map.put("user", userService.findUserById(post.getUserId()));
