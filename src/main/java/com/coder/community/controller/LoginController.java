7f430b7aa94850890729856ea71077966d3c0a4b
kong <376634352@qq.com>
2023-05-16 17:18:02 +0800

    添加功能：生成验证吗

--- src/main/java/com/coder/community/controller/LoginController.java
+++ src/main/java/com/coder/community/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.coder.community.controller;
 import com.coder.community.entity.User;
 import com.coder.community.service.UserService;
 import com.coder.community.utils.CommunityConstant;
+import com.google.code.kaptcha.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,17 +13,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
+
 
     @GetMapping("/register")
     public String getRegisterPage() {
         return "/site/register";
     }
+
     @GetMapping("/login")
     public String getLoginPage() {
         return "/site/login";
@@ -43,6 +58,7 @@ public class LoginController implements CommunityConstant {
 
     /**
      * 激活函数
+     *
      * @param model
      * @param userId
      * @param code
@@ -50,7 +66,7 @@ public class LoginController implements CommunityConstant {
      */
     //    http://localhost:8080/community/activation/101/code
     @GetMapping("/activation/{userId}/{code}")
-    public String activation(Model model, @PathVariable("userId") int userId,@PathVariable("code") String code) {
+    public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code) {
         int result = userService.activation(userId, code);
         if (result == ACTIVATION_SUCCESS) {
             model.addAttribute("msg", "激活成功，您可以正常使用账户");
@@ -66,5 +82,22 @@ public class LoginController implements CommunityConstant {
 
         }
         return "/site/operate-result";
+    }
+
+    @GetMapping("/kaptcha")
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        // 生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+//        将验证码存入session
+        session.setAttribute("kaptcha", text);
+//        将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            ServletOutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            logger.error("响应验证码失败" + e.getMessage());
+        }
     }
 }
